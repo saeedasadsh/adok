1e268ab10f0e8e56ebca7bc0a822ee2b595e49de
saeed <saeedasadsh@gmail.com>
2017-12-12 15:26:04 +0330

    Add files via upload

---
+++ notificationServer.js
@@ -0,0 +1,16 @@
+var server = require('http').Server();
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    console.log("connected");
+    
+  socket.on('message', function(data){
+    console.log("Received message : "+data);
+    socket.emit("message","echoing back : "+data);
+  });
+    
+  socket.on('disconnect', function(){
+    console.log("disconnected");
+   });
+});
+server.listen(3010);
