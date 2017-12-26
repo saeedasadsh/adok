24c05b03ad1e03b40a7fabe8fb5047e1602c09a0
saeed <saeedasadsh@gmail.com>
2017-12-26 11:53:02 +0330

    123

--- AdokNotification/AdokNotification/server.js
+++ AdokNotification/AdokNotification/server.js
@@ -11,135 +11,142 @@ console.log("server started");
 
 
 (function () {
-    var c = 0;
-    var timeout = setInterval(function () {
 
-        var dataQS = {
-            var1: "something",
-            var2: "something else"
-        };
+    try {
+        var c = 0;
+        var timeout = setInterval(function () {
 
-        var querystring = require("querystring");
-        var qs = querystring.stringify(dataQS);
-        var qslength = qs.length;
+            var dataQS = {
+                var1: "something",
+                var2: "something else"
+            };
 
-        var options = {
-            hostname: "ashabrasaneh.ir",
-            port: 80,
-            path: "/GamesData/ADok/GetNotifications.php",
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': qslength
-            }
-        };
+            var querystring = require("querystring");
+            var qs = querystring.stringify(dataQS);
+            var qslength = qs.length;
 
-        var buffer = "";
-        var req = http.request(options, function (res) {
-            res.on('data', function (chunk) {
-                buffer += chunk;
-            });
+            var options = {
+                hostname: "ashabrasaneh.ir",
+                port: 80,
+                path: "/GamesData/ADok/GetNotifications.php",
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': qslength
+                }
+            };
 
-            res.on('end', function () {
+            var buffer = "";
+            var req = http.request(options, function (res) {
+                res.on('data', function (chunk) {
+                    buffer += chunk;
+                });
 
-                try {
-                    var dt = JSON.parse(buffer);
-                    //console.log(dt);
-                    for (var i = 0; i < dt.length; i++) {
-                        var id = dt[i].id;
-                        if (id != -1) {
-                            var appId = dt[i].appId;
-                            var title = dt[i].title;
-                            var message = dt[i].message;
-                            var url = dt[i].url;
-                            var timeToLive = dt[i].timeToLive;
-                            var dateStartSend = dt[i].dateStartSend;
-                            var timeStartSend = dt[i].timeStartSend;
-                            var sound = dt[i].sound;
-                            var smalIcon = dt[i].smalIcon;
-                            var largeIcon = dt[i].largeIcon;
-                            var bigPicture = dt[i].bigPicture;
-                            var ledColor = dt[i].ledColor;
-                            var accentColor = dt[i].accentColor;
-                            var gId = dt[i].gId;
-                            var priority = dt[i].priority;
-                            var pkgNameAndroid = dt[i].pkgNameAndroid;
-                            var pkgNameIos = dt[i].pkgNameIos;
-                            var AdditionalData = dt[i].AdditionalData;
-                            var btns = dt[i].btns;
-                            var players = [];
-                            var playersId = [];
+                res.on('end', function () {
 
-                            var userData = {
-                                id: id, appId: appId, title: title, message: message, url: url, timeToLive: timeToLive
-                                , dateStartSend: dateStartSend, timeStartSend: timeStartSend, sound: sound, smalIcon: smalIcon, largeIcon: largeIcon
-                                , bigPicture: bigPicture, ledColor: ledColor, accentColor: accentColor, gId: gId, priority: priority
-                                , pkgNameAndroid: pkgNameAndroid, pkgNameIos: pkgNameIos, AdditionalData: AdditionalData, btns: btns, players: players, playersId: playersId
-                            };
+                    try {
+                        var dt = JSON.parse(buffer);
+                        //console.log(dt);
+                        for (var i = 0; i < dt.length; i++) {
+                            var id = dt[i].id;
+                            if (id != -1) {
+                                var appId = dt[i].appId;
+                                var title = dt[i].title;
+                                var message = dt[i].message;
+                                var url = dt[i].url;
+                                var timeToLive = dt[i].timeToLive;
+                                var dateStartSend = dt[i].dateStartSend;
+                                var timeStartSend = dt[i].timeStartSend;
+                                var sound = dt[i].sound;
+                                var smalIcon = dt[i].smalIcon;
+                                var largeIcon = dt[i].largeIcon;
+                                var bigPicture = dt[i].bigPicture;
+                                var ledColor = dt[i].ledColor;
+                                var accentColor = dt[i].accentColor;
+                                var gId = dt[i].gId;
+                                var priority = dt[i].priority;
+                                var pkgNameAndroid = dt[i].pkgNameAndroid;
+                                var pkgNameIos = dt[i].pkgNameIos;
+                                var AdditionalData = dt[i].AdditionalData;
+                                var btns = dt[i].btns;
+                                var players = [];
+                                var playersId = [];
 
-                            var canAdd = 0;
-                            if (id > 0) {
-                                for (var i = 0; i < rooms.length; i++) {
-                                    if (rooms[i].id == id) {
-                                        canAdd = 1;
+                                var userData = {
+                                    id: id, appId: appId, title: title, message: message, url: url, timeToLive: timeToLive
+                                    , dateStartSend: dateStartSend, timeStartSend: timeStartSend, sound: sound, smalIcon: smalIcon, largeIcon: largeIcon
+                                    , bigPicture: bigPicture, ledColor: ledColor, accentColor: accentColor, gId: gId, priority: priority
+                                    , pkgNameAndroid: pkgNameAndroid, pkgNameIos: pkgNameIos, AdditionalData: AdditionalData, btns: btns, players: players, playersId: playersId
+                                };
+
+                                var canAdd = 0;
+                                if (id > 0) {
+                                    for (var i = 0; i < rooms.length; i++) {
+                                        if (rooms[i].id == id) {
+                                            canAdd = 1;
+                                        }
+                                    }
+                                    if (canAdd == 0) {
+                                        rooms.push(userData);
+                                        console.log("rooms added- rooms count:" + rooms.length);
                                     }
                                 }
-                                if (canAdd == 0) {
-                                    rooms.push(userData);
-                                    console.log("rooms added- rooms count:" + rooms.length);
-                                }
                             }
                         }
-                    }
 
-                    var today = new Date();
-                    var h = today.getHours();
-                    var m = today.getMinutes();
+                        var today = new Date();
+                        var h = today.getHours();
+                        var m = today.getMinutes();
 
-                    var dat = h * 60 + m;
+                        var dat = h * 60 + m;
 
-                    rooms.forEach(function (item, index, object) {
-                        if (item.timeStartSend + item.timeToLive < dat) {
-                            object.splice(index, 1);
-                        }
-                    });
-
-                    console.log("sending notification to " + rooms.length + " apps");
-
-                    rooms.forEach(function (item, index, object) {
-                        var noti = {
-                            id: item.id, appId: item.appId, title: item.title, message: item.message, url: item.url, timeToLive: item.timeToLive
-                            , dateStartSend: item.dateStartSend, timeStartSend: item.timeStartSend, sound: item.sound, smalIcon: item.smalIcon, largeIcon: item.largeIcon
-                            , bigPicture: item.bigPicture, ledColor: item.ledColor, accentColor: item.accentColor, gId: item.gId, priority: item.priority
-                            , pkgNameAndroid: item.pkgNameAndroid, pkgNameIos: item.pkgNameIos, AdditionalData: item.AdditionalData, btns: item.btns
-                        };
-
-
-                        item.players.forEach(function (itemp, indexp, objectp) {
-                            try {
-                                if (itemp != undefined) {
-                                    itemp.write(JSON.stringify(noti) + "\n");
-                                }
-                            }
-                            catch (e) {
-                                console.log("error 1 " + e);
-                                objectp.splice(indexp, 1);
+                        rooms.forEach(function (item, index, object) {
+                            if (item.timeStartSend + item.timeToLive < dat) {
+                                object.splice(index, 1);
                             }
                         });
 
-                    });
-                }
-                catch (e) {
-                    console.log("error 2 " + e);
-                }
+                        console.log("sending notification to " + rooms.length + " apps");
+
+                        rooms.forEach(function (item, index, object) {
+                            var noti = {
+                                id: item.id, appId: item.appId, title: item.title, message: item.message, url: item.url, timeToLive: item.timeToLive
+                                , dateStartSend: item.dateStartSend, timeStartSend: item.timeStartSend, sound: item.sound, smalIcon: item.smalIcon, largeIcon: item.largeIcon
+                                , bigPicture: item.bigPicture, ledColor: item.ledColor, accentColor: item.accentColor, gId: item.gId, priority: item.priority
+                                , pkgNameAndroid: item.pkgNameAndroid, pkgNameIos: item.pkgNameIos, AdditionalData: item.AdditionalData, btns: item.btns
+                            };
+
+
+                            item.players.forEach(function (itemp, indexp, objectp) {
+                                try {
+                                    if (itemp != undefined) {
+                                        itemp.write(JSON.stringify(noti) + "\n");
+                                    }
+                                }
+                                catch (e) {
+                                    console.log("error 1 " + e);
+                                    objectp.splice(indexp, 1);
+                                }
+                            });
+
+                        });
+                    }
+                    catch (e) {
+                        console.log("error 2 " + e);
+                    }
+                });
+
             });
 
-        });
+            req.write(qs);
+            req.end();
 
-        req.write(qs);
-        req.end();
-
-    }, 10000);
+        }, 10000);
+    }
+    catch (e) {
+        //delete sockets[i];
+        console.log("error 5 " + e);
+    }
 })();
 
 var decoder = new StringDecoder('utf8');
@@ -195,13 +202,12 @@ server.on('connection', function (socket) {
                 }
             }
 
-            if (added==0)
-            {
+            if (added == 0) {
                 var mes = {
                     id: "close"
                 };
 
-                socket.write(JSON.stringify(mes)+"\n");
+                socket.write(JSON.stringify(mes) + "\n");
             }
         }
         catch (e) {
