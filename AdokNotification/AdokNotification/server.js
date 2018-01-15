a0e6ac096446db86c075c5c72148c7e3d61d6494
saeed <saeedasadsh@gmail.com>
2018-01-16 02:12:11 +0330

    1234

--- AdokNotification/AdokNotification/server.js
+++ AdokNotification/AdokNotification/server.js
@@ -124,7 +124,7 @@ console.log("server started");
                             item.players.forEach(function (itemp, indexp, objectp) {
                                 try {
                                     if (itemp != undefined) {
-                                        //console.log('noti to: ' + itemp.remoteAddress + ':' + itemp.remotePort);
+                                        console.log('noti to: ' + itemp.remoteAddress + ':' + itemp.remotePort);
                                         itemp.write(JSON.stringify(noti) + "\n");
                                     }
                                 }
