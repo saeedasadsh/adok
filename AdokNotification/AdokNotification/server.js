99b37127c6619f4831c1b16cf22eace0f064329e
saeed <saeedasadsh@gmail.com>
2018-07-05 06:57:23 +0430

    123

--- AdokNotification/AdokNotification/server.js
+++ AdokNotification/AdokNotification/server.js
@@ -498,7 +498,29 @@ function PlayerDisonnected(pid) {
 function GetNotifications() {
     con.connect(function (err) {
         if (err) throw err;
-        var query = "SELECT notification.id,notification.appId,notification.title,notification.message,notification.url,notification.timeToLive,notification.dateStartSend,notification.timeStartSend,notification.sound, notification.smalIcon, notification.largeIcon, notification.bigPicture, notification.ledColor, notification.accentColor, notification.gId, notification.priority, apps.pkgNameAndroid, apps.pkgNameIos, notification.kind, notification.IsStop, notification.lastUpdateTime, notification.bigText, notification.summary, notification.budget, notification.isTest, notification.playerId FROM notification  inner join apps on notification.appId = apps.id where dateStartSend= $dateHejri and notification.isSend = 0;";
+        var d = new Date();
+        var y = d.getFullYear();
+        var m = d.getMonth();
+        m++;
+        var day = d.getDate();
+        var dateHijri = gregorian_to_jalali(y, m, d);
+        y = dateHijri[0];
+        m = dateHijri[0];
+        day = dateHijri[0];
+
+        if (m.length == 1)
+        {
+            m = "0" + m;
+        }
+
+        if (day.length == 1) {
+            day = "0" + day;
+        }
+
+        var curDate = y + "" + m + "" + d;
+        console.log(curDate);
+
+        var query = "SELECT notification.id,notification.appId,notification.title,notification.message,notification.url,notification.timeToLive,notification.dateStartSend,notification.timeStartSend,notification.sound, notification.smalIcon, notification.largeIcon, notification.bigPicture, notification.ledColor, notification.accentColor, notification.gId, notification.priority, apps.pkgNameAndroid, apps.pkgNameIos, notification.kind, notification.IsStop, notification.lastUpdateTime, notification.bigText, notification.summary, notification.budget, notification.isTest, notification.playerId FROM notification  inner join apps on notification.appId = apps.id where dateStartSend=" +curDate+" and notification.isSend = 0;";
         con.query(query, function (err, result, fields) {
             if (err) throw err;
             console.log(fields.length);
@@ -521,4 +543,60 @@ function db()
 
         });
     });
+}
+
+function gregorian_to_jalali(gy, gm, gd) {
+    g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    if (gy > 1600) {
+        jy = 979;
+        gy -= 1600;
+    } else {
+        jy = 0;
+        gy -= 621;
+    }
+    gy2 = (gm > 2) ? (gy + 1) : gy;
+    days = (365 * gy) + (parseInt((gy2 + 3) / 4)) - (parseInt((gy2 + 99) / 100)) + (parseInt((gy2 + 399) / 400)) - 80 + gd + g_d_m[gm - 1];
+    jy += 33 * (parseInt(days / 12053));
+    days %= 12053;
+    jy += 4 * (parseInt(days / 1461));
+    days %= 1461;
+    if (days > 365) {
+        jy += parseInt((days - 1) / 365);
+        days = (days - 1) % 365;
+    }
+    jm = (days < 186) ? 1 + parseInt(days / 31) : 7 + parseInt((days - 186) / 30);
+    jd = 1 + ((days < 186) ? (days % 31) : ((days - 186) % 30));
+    return [jy, jm, jd];
+}
+
+
+function jalali_to_gregorian(jy, jm, jd) {
+    if (jy > 979) {
+        gy = 1600;
+        jy -= 979;
+    } else {
+        gy = 621;
+    }
+    days = (365 * jy) + ((parseInt(jy / 33)) * 8) + (parseInt(((jy % 33) + 3) / 4)) + 78 + jd + ((jm < 7) ? (jm - 1) * 31 : ((jm - 7) * 30) + 186);
+    gy += 400 * (parseInt(days / 146097));
+    days %= 146097;
+    if (days > 36524) {
+        gy += 100 * (parseInt(--days / 36524));
+        days %= 36524;
+        if (days >= 365) days++;
+    }
+    gy += 4 * (parseInt(days / 1461));
+    days %= 1461;
+    if (days > 365) {
+        gy += parseInt((days - 1) / 365);
+        days = (days - 1) % 365;
+    }
+    gd = days + 1;
+    sal_a = [0, 31, ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    for (gm = 0; gm < 13; gm++) {
+        v = sal_a[gm];
+        if (gd <= v) break;
+        gd -= v;
+    }
+    return [gy, gm, gd];
 }
