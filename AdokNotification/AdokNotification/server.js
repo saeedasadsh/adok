f51aa1057e725ef76ac020e8c52062c381af26a1
saeed <saeedasadsh@gmail.com>
2018-07-05 07:52:06 +0430

    123

--- AdokNotification/AdokNotification/server.js
+++ AdokNotification/AdokNotification/server.js
@@ -556,7 +556,7 @@ function GetNotifications() {
         var query = "SELECT notification.id,notification.appId,notification.title,notification.message,notification.url,notification.timeToLive,notification.dateStartSend,notification.timeStartSend,notification.sound, notification.smalIcon, notification.largeIcon, notification.bigPicture, notification.ledColor, notification.accentColor, notification.gId, notification.priority, apps.pkgNameAndroid, apps.pkgNameIos, notification.kind, notification.IsStop, notification.lastUpdateTime, notification.bigText, notification.summary, notification.budget, notification.isTest, notification.playerId FROM notification  inner join apps on notification.appId = apps.id where dateStartSend>=" + curDate + " and notification.isSend = 0;";
         con.query(query, function (err, result, fields) {
             if (err) throw err;
-            rows.forEach((row) => {
+            result.forEach((row) => {
                 console.log("notiId: " + result.id);
                 var id = result.id;
                 var appId = result.appId;
@@ -594,6 +594,7 @@ function GetNotifications() {
                     bigText: result.bigText, summary: result.summary, AdditionalData: result.AdditionalData, btns: result.btns, Meskind: "noti"
                 };
 
+                console.log(noti);
                 if (isTest > 0) {
                     if (pkgNameAndroid != "") {
                         if (Players[pkgNameAndroid] != undefined) {
